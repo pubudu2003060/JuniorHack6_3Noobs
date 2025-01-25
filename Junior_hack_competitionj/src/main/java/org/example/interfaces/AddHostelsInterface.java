7175b22ad55a50160cc3230b7f6784b9d60f7b71
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-01-25 12:14:08 +0530

    finish

--- Junior_hack_competitionj/src/main/java/org/example/interfaces/AddHostelsInterface.java
+++ Junior_hack_competitionj/src/main/java/org/example/interfaces/AddHostelsInterface.java
@@ -1,5 +1,10 @@
 package org.example.interfaces;
 
+import org.example.DTO.HostelsDTO;
+import org.example.databaseConnection.DatabaseConnection;
+import org.example.operation.AddOperationHostel;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AddHostelsInterface {
@@ -12,6 +17,18 @@ public class AddHostelsInterface {
         String hostelID = sc.nextLine();
         System.out.println("Enter number of Hostel Rooms: ");
         int numberOfHostelRooms = sc.nextInt();
+
+        HostelsDTO hostel = new HostelsDTO(hostelID, hostelName, numberOfHostelRooms);
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        AddOperationHostel addOperationHostel = new AddOperationHostel();
+
+        try {
+            String result = (String) addOperationHostel.operation(databaseConnection.getConnection(),hostel);
+            System.out.println(result);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
 
         return 1;
     }
