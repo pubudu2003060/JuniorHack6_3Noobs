0ea4151e5a5d14d63c585d02c3b1df514cacb5df
rashmika <rashmikanethsarani119@gmail.com>
2025-01-25 10:47:01 +0530

    Created Remove Student Interface

---
+++ Junior_hack_competitionj/src/main/java/org/example/interfaces/RemoveStudentInterface.java
@@ -0,0 +1,21 @@
+package org.example.interfaces;
+
+import java.util.Scanner;
+
+public class RemoveStudentInterface {
+    public String craeteRemoveStudentInterface() {
+        System.out.println("Removing a student....\n");
+        Scanner option=new Scanner(System.in);
+        System.out.println("Student Id");
+        int studentId=option.nextInt();
+        System.out.println("\n");
+        System.out.println("Do you want to remove student " +studentId+ "? (Y/N)");
+        String answer=option.next();
+        option.nextLine();
+
+        return answer;
+
+
+
+    }
+}
