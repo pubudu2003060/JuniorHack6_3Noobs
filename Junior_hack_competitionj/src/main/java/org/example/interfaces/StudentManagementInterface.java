768fe1230c2af174e44d60b2aecff59ee4b4d177
rashmika <rashmikanethsarani119@gmail.com>
2025-01-25 10:12:57 +0530

    Created the Student management menu interface

---
+++ Junior_hack_competitionj/src/main/java/org/example/interfaces/StudentManagementInterface.java
@@ -0,0 +1,20 @@
+package org.example.interfaces;
+
+import java.util.Scanner;
+
+public class StudentManagementInterface {
+    public int createStudentManagement(){
+        Scanner Option = new Scanner(System.in);
+
+        System.out.println("Student management...\n\n");
+        System.out.println("[1] Add student \n");
+        System.out.println("[2] Remove Students\n");
+        System.out.println("[3] Update Student\n");
+        System.out.println("[4] View Student Details\n");
+        System.out.println("[0] Exit\n");
+
+        int optionStudentManagemt = Option.nextInt();
+
+        return optionStudentManagemt;
+    }
+}
